1734f0f633ad9161e6e8a56c3940edadcb9468d8
rajatswaroop <rajatsrivastava502@gmail.com>
2021-06-17 13:03:55 +0530

    Updated Promise
    Updated with action on Success/Failure to keep promise.

--- promise.js
+++ promise.js
@@ -1,13 +1,23 @@
 let done = true
 /*
+Promise = Real Life behaviour of Promising. Either the promise is rejectd or resolved.
+
 So the promise is node is also the asynchronous response , which either resolves ior reject based on the condition.
+
+Promise was launched to replace callback hll problem, which is further replacd by Asunc/Await.(Thanks to the faster evolving javascript.)
  */
-const isItDoneYet = new Promise((resolve, reject) => {
+let p = new Promise((resolve, reject) => {
     if (done) {
-        const workDone = 'Here is the thing I built'
+        const workDone = 'I am felling great about Node js'
         resolve(workDone)
     } else {
-        const why = 'Still working on something else'
+        const why = 'Alright Java is multiThreaded.....'
         reject(why)
     }
+})
+
+p.then((message) => {
+console.log('Keep Learning Node js----->'+message)
+}).catch((message) => {
+    console.log('Java has different benefits'+message)
 })
